af701852a5699184c393c69dc5b9f92ba78e9d89
Jesus Martinez <jesus258911@gmail.com>
2024-01-14 19:18:14 -0500

    agregando la escucha de los botones para movimiento

--- game.js
+++ game.js
@@ -1,5 +1,21 @@
+    // game.fillRect(100,25,100,100); //tamaño del canvas
+    // game.clearRect(125,50,50,50) // borrar parte del canvas
+
+    // game.font= '15px serif'; //tamaño y color de la fuente
+    // game.fillStyle = 'green' // color de la fuente
+    // game.fillText('PLATZI', 125,80); //colocamos algin 
+
+
+
 const canvas = document.querySelector('#game'); //seleccionamos cuadro que contendra el tablero
 const game = canvas.getContext('2d'); //le decimos que en ese elemento lo convertiremos en un canvas o hoja de dibujo
+
+const btnUp = document.querySelector('#up');
+const btnLeft = document.querySelector('#left');
+const btnRight = document.querySelector('#right');
+const btnDown = document.querySelector('#down');
+
+
 
 window.addEventListener('load', setCanvasSize); // que cargue el juego luego de haber cargado la pagina o el html para evitar futuros errores
 window.addEventListener('resize', setCanvasSize);
@@ -23,19 +39,24 @@ function startGame(){
 
     console.log(map,mapRows,mapRowCols);
 
-    for( let i = 0; i < 10; i++){
-        for(let j=0; j < 10; j++){
-         console.log (i,j);
-            game.fillText(emojis[mapRowCols[j][i]], elementsSize  * (i+0), elementsSize * (j+1));
-        }
-    }
+    const imprimirMaps = mapRowCols.forEach( (row, rowI) => {   
+        row.forEach( (col, colI) => {
+            const emoji = emojis[col];
+            const posX = elementsSize * (colI);
+            const posY = elementsSize * (rowI+1);
+            game.fillText(emoji, posX, posY);
+        })
         
-    // game.fillRect(100,25,100,100); //tamaño del canvas
-    // game.clearRect(125,50,50,50) // borrar parte del canvas
+    });
 
-    // game.font= '15px serif'; //tamaño y color de la fuente
-    // game.fillStyle = 'green' // color de la fuente
-    // game.fillText('PLATZI', 125,80); //colocamos algin texto
+    // for( let row = 0; row < 10; row++){
+    //     for(let col=0; col < 10; col++){
+
+    //         game.fillText(emojis[mapRowCols[row][col]], elementsSize  * (col), elementsSize * (row+1));
+    //     }
+    // }
+        
+ 
 }
 
 function setCanvasSize(){
@@ -55,4 +76,46 @@ function setCanvasSize(){
 
      startGame();
 
+}
+
+// EVENTOS DE MOVIMIENTO PARA EL JUGADOR.
+
+window.addEventListener('keydown',moveByKeys)
+
+btnUp.addEventListener('click', moveUp);
+btnDown.addEventListener('click', moveDown);
+btnLeft.addEventListener('click', moveLeft);
+btnRight.addEventListener('click', moveRight);
+
+
+function moveByKeys(event){
+    
+    switch (event.key) {
+        case "ArrowUp":
+            moveUp();
+            break;
+        case "ArrowDown":
+            moveDown();
+            break;
+        case "ArrowLeft":
+            moveLeft();
+            break;
+        case "ArrowRight":
+            moveRight();
+            break;
+    }
+
+
+}
+function moveUp(){
+    console.log('Me muevo hacia arriba');
+}
+function moveDown(){
+    console.log('Me muevo hacia abajo');
+}
+function moveLeft(){
+    console.log('Me muevo hacia izquieda');
+}
+function moveRight(){
+    console.log('Me muevo hacia derecha');
 }
